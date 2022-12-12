5b96f01c91b972c93acd4bbe860fbd87c9d82c2e
shirs12 <shirseg98@gmail.com>
2022-12-12 17:24:11 +0200

    update

---
+++ Exercise03.java
@@ -0,0 +1,23 @@
+public class Exercise03 {
+    /*
+    https://edabit.com/challenge/GJh9FraKutcnHoruX
+
+    Find the Missing Number
+    Create a method that takes an array of integers between 1 and 10,
+    (excluding one number) and returns the missing number.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int missingNum(int[] arr) {
+        int[] check = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            if ()
+        }
+
+        return
+    }
+
+}
