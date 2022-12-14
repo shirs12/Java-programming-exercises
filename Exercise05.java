8095742436ebb67e0c10ff64f868f40ed06b84d9
shirs12 <shirseg98@gmail.com>
2022-12-14 20:13:11 +0200

    update

---
+++ Exercise05.java
@@ -0,0 +1,40 @@
+public class Exercise05 {
+    /*
+    https://edabit.com/challenge/KESQCRf5mJd7x6Rfh
+
+    Remove Duplicates from an Array
+    Create a method that takes an array of strings or integers,
+    removes all duplicate items and returns a new array,
+    in the same sequential order as the old array
+    (minus duplicates).
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] removeDups(int[] arr) {
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        return
+    }
+    public static String[] removeDups(String[] arr) {
+        String[] newArr = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        return
+    }
+
+
+}
+
+
+
+
+
+
+
+
