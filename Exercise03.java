f980bd3128aabce84db02349df91465dc50607e7
shirs12 <shirseg98@gmail.com>
2022-12-14 13:17:54 +0200

    added mergeSort method

--- Exercise03.java
+++ Exercise03.java
@@ -8,10 +8,23 @@ public class Exercise03 {
      */
 
     public static void main(String[] args) {
+        int[] numbers = {3,9,5,8,1,6,2,7,4};
+        printArray(numbers);
+        mergeSort(numbers);
+        printArray(numbers);
+    }
 
+    public static void printArray(int[] array){
+        System.out.print("[");
+        for (int i = 0; i < array.length-1; i++) {
+            System.out.print(array[i] + ",");
+        }
+        System.out.print(array[array.length-1] + "]");
+        System.out.println();
     }
 
 //    public static int missingNum(int[] arr) {
+//        mergeSort(arr);
 //        int[] check = new int[10];
 //        for (int i = 0; i < arr.length; i++) {
 //            if ()
@@ -19,5 +32,52 @@ public class Exercise03 {
 //
 //        return
 //    }
+
+    // Complexity: O(n log n)
+    public static void mergeSort(int[] arr) {
+        int len = arr.length;
+        if (len < 2)
+            return;
+        int mid = len / 2;
+        int[] left = new int[mid];
+        int[] right = new int[len - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < arr.length; i++) {
+            right[i - mid] = arr[i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+    private static void merge(int[] arr, int[] left, int[] right){
+        int leftSize = left.length;
+        int rightSize = right.length;
+        int i = 0, j = 0, k = 0;
+        while (i < leftSize && j < rightSize){
+            if (left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSize){
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < rightSize){
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
 
 }
